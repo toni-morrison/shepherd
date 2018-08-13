79468c02ca6c0e48957fc08619107a223880b1b6
Kiernan Hahn <kiernanhahn@gmail.com>
2018-08-13 19:28:05 -0400

    Got rid of bloat in MVP of calendar

--- client/UserCalendar/UserCalendar.jsx
+++ client/UserCalendar/UserCalendar.jsx
@@ -12,40 +12,14 @@ class UserCalendar extends React.Component {
     }
   }
   
-  function initClient() {
-    gapi.client.init({
-      apiKey: this.state.API_KEY,
-      clientId: this.state.CLIENT_ID,
-      discoveryDocs: this.state.DISCOVERY_DOCS,
-      scope: this.state.SCOPES
-    }).then(function () {
-      // Listen for sign-in state changes.
-      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-      // Handle the initial sign-in state.
-      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-
-    });
-  }
-  
-  function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-  }
-  
   componentDidMount () {
     
   }
   
   render () {
     return (<div>
-      <iframe src={this.state.source} style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <iframe src="https://calendar.google.com/calendar/embed?src=4ggtmdojqb8je3n6mlnicj02jc%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
-      <script async defer src="https://apis.google.com/js/api.js"
-        onload={this.onload=function(){};this.handleClientLoad()}
-        onreadystatechange={if (this.readyState === 'complete') this.onload()}>
-      </script>
-      
-      
       </div>)
   }
 }
