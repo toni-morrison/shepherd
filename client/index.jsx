9c1f364b6487557d8ac3ad3c5af07b30bac97182
Debbie Lee <ydebbielee@gmail.com>
2018-08-13 19:31:18 -0400

    refactor: Pushing index.jsx and package files

--- client/index.jsx
+++ client/index.jsx
@@ -4,7 +4,9 @@ import axios from 'axios';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider, Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import TopTabs from './Tabs/TopTabs.jsx'
+import TopTabs from './Tabs/TopTabs.jsx';
+import Login from './Login/Login.jsx';
+import Signup from './Signup/Signup.jsx';
 
 const client = new ApolloClient();
 
