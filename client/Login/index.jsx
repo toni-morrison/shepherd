bdfe444631bb529c828c6b03c5f7dc0942c2a238
Dan Guan <daniel.guan.bca@gmail.com>
2018-08-13 13:43:22 -0400

    Rename existing components
    - Rename components to folder names
    - Add ApolloClient to index.jsx

--- client/Login/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-class ___ extends React.Component {
-  constructor (props) {
-    super(props)
-  }
-  render () {
-    return (<div></div>)
-  }
-}
---
+++ client/SplashPage/SplashPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class SplashPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div />;
+  }
+}
