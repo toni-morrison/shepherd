87d37696c79a505c52fecacb5a235b43403aa1e7
Kiernan Hahn <kiernanhahn@gmail.com>
2018-08-14 19:29:57 -0400

    Closing branch

--- client/UserCalendar/UserCalendar.jsx
+++ client/UserCalendar/UserCalendar.jsx
@@ -13,12 +13,6 @@ export default class UserCalendar extends React.Component {
       modalShow: false,
       currentEvent: {}
     }
-//    this.state = {
-//      API_KEY: 'AIzaSyA3tNjW2qXiX07_v3rpKhWvN_CKfkUNqfc',
-//      CLIENT_ID: '667292252749-a250ctrk4m90rt4hu3iflie2lot9splk.apps.googleusercontent.com',
-//      SCOPES: ["https://www.googleapis.com/auth/calendar"],
-//      DISCOVERY_DOCS: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
-//    }
   BigCalendar.setLocalizer(BigCalendar.momentLocalizer (moment))
   this.allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
   this.handleClose = this.handleClose.bind(this)
