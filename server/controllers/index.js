820618c18197d0a7271689b5415b0134b5b84aa5
Kiernan Hahn <kiernanhahn@gmail.com>
2018-08-14 19:52:25 -0400

    Adjusted dependencies and copied the calendar's CSS to the main CSS file

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static(__dirname + '/../../client'));
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log('Listening on port: ', port));
