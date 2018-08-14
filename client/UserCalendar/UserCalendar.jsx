47b453501b50d4a7c74f4df708a7a68d65435464
Kiernan Hahn <kiernanhahn@gmail.com>
2018-08-14 17:25:15 -0400

    Equipped the Calendar and a modal for events

--- client/UserCalendar/UserCalendar.jsx
+++ client/UserCalendar/UserCalendar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import BigCalendar from 'react-big-calendar'
+import {Modal} from 'react-bootstrap'
 import moment from 'moment'
 import dates from './dates.js'
 import events from './events.js'
+import AppointmentModal from './AppointmentModal.jsx'
 export default class UserCalendar extends React.Component {
   constructor (props) {
     super(props)
+    
+    this.state = {
+      modalShow: false,
+      currentEvent: {}
+    }
 //    this.state = {
 //      API_KEY: 'AIzaSyA3tNjW2qXiX07_v3rpKhWvN_CKfkUNqfc',
 //      CLIENT_ID: '667292252749-a250ctrk4m90rt4hu3iflie2lot9splk.apps.googleusercontent.com',
@@ -14,8 +21,20 @@ export default class UserCalendar extends React.Component {
 //    }
   BigCalendar.setLocalizer(BigCalendar.momentLocalizer (moment))
   this.allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
+  this.handleClose = this.handleClose.bind(this)
+  this.handleSelect = this.handleSelect.bind(this)
   }
-
+  
+  handleSelect (event) {
+    this.setState({currentEvent: event})
+    this.setState ({modalShow: true})
+  }
+  
+  handleClose () {
+    this.setState({modalShow: false})
+  }
+  
+  
 
   render () {
     return (<div>
@@ -25,10 +44,9 @@ export default class UserCalendar extends React.Component {
         step={60}
         showMultiDayTimes
         selectable
-        onSelectEvent={event => alert(event.sitterID)}
-
+        onSelectEvent={this.handleSelect}
       />
-
+      <AppointmentModal show = {this.state.modalShow} handleClose = {this.handleClose} currentEvent = {this.state.currentEvent}/>
       </div>)
   }
 }
--- client/UserCalendar/events.js
+++ client/UserCalendar/events.js
@@ -1,108 +1,34 @@
 export default [
   {
     id: 0,
-    title: 'All Day Event very long title',
+    title: 'John, HouseSitting',
     allDay: true,
     start: new Date('2018-08-15T14:20:00'),
     end: new Date('2018-08-15T16:24:00'),
-    userID: 'userID1',
-    sitterID: 'sitterID1',
-    appointmentID: 'appointment1'
+    userID: 'userID0',
+    sitterID: 'sitterID0',
+    appointmentID: 'appointment0',
+    status: 'PENDING',
+    username: 'John Housesitter',
+    cost: 300,
+    instructions: ['Take out the garbage',
+                   'Mow the lawn',
+                   'Dust the bookshelf']
   },
   {
     id: 1,
-    title: 'All Day Event very long title',
-    allDay: true,
-    start: new Date('2018-08-14T10:10:00'),
+    title: 'Jane, PetSitting',
+    allDay: false,
+    start: new Date('2018-08-14T11:10:00'),
     end: new Date('2018-08-14T12:12:00'),
     userID: 'userID1',
     sitterID: 'sitterID1',
-    appointmentID: 'appointment1'
-  },
-
-  {
-    id: 2,
-    title: 'DTS STARTS',
-    start: new Date(2016, 2, 13, 0, 0, 0),
-    end: new Date(2016, 2, 20, 0, 0, 0),
-  },
-
-  {
-    id: 3,
-    title: 'DTS ENDS',
-    start: new Date(2016, 10, 6, 0, 0, 0),
-    end: new Date(2016, 10, 13, 0, 0, 0),
-  },
-
-  {
-    id: 4,
-    title: 'Some Event',
-    start: new Date(2015, 3, 9, 0, 0, 0),
-    end: new Date(2015, 3, 10, 0, 0, 0),
-  },
-  {
-    id: 5,
-    title: 'Conference',
-    start: new Date(2015, 3, 11),
-    end: new Date(2015, 3, 13),
-    desc: 'Big conference for important people',
-  },
-  {
-    id: 6,
-    title: 'Meeting',
-    start: new Date(2015, 3, 12, 10, 30, 0, 0),
-    end: new Date(2015, 3, 12, 12, 30, 0, 0),
-    desc: 'Pre-meeting meeting, to prepare for the meeting',
-  },
-  {
-    id: 7,
-    title: 'Lunch',
-    start: new Date(2015, 3, 12, 12, 0, 0, 0),
-    end: new Date(2015, 3, 12, 13, 0, 0, 0),
-    desc: 'Power lunch',
-  },
-  {
-    id: 8,
-    title: 'Meeting',
-    start: new Date(2015, 3, 12, 14, 0, 0, 0),
-    end: new Date(2015, 3, 12, 15, 0, 0, 0),
-  },
-  {
-    id: 9,
-    title: 'Happy Hour',
-    start: new Date(2015, 3, 12, 17, 0, 0, 0),
-    end: new Date(2015, 3, 12, 17, 30, 0, 0),
-    desc: 'Most important meal of the day',
-  },
-  {
-    id: 10,
-    title: 'Dinner',
-    start: new Date(2015, 3, 12, 20, 0, 0, 0),
-    end: new Date(2015, 3, 12, 21, 0, 0, 0),
-  },
-  {
-    id: 11,
-    title: 'Birthday Party',
-    start: new Date(2015, 3, 13, 7, 0, 0),
-    end: new Date(2015, 3, 13, 10, 30, 0),
-  },
-  {
-    id: 12,
-    title: 'Late Night Event',
-    start: new Date(2015, 3, 17, 19, 30, 0),
-    end: new Date(2015, 3, 18, 2, 0, 0),
-  },
-  {
-    id: 12.5,
-    title: 'Late Same Night Event',
-    start: new Date(2015, 3, 17, 19, 30, 0),
-    end: new Date(2015, 3, 17, 23, 30, 0),
-  },
-  {
-    id: 13,
-    title: 'Multi-day Event',
-    start: new Date(2015, 3, 20, 19, 30, 0),
-    end: new Date(2015, 3, 22, 2, 0, 0),
+    appointmentID: 'appointment1',
+    status: 'ACCEPTED',
+    username: 'Jane Petsitter',
+    cost: 100,
+    instructions: ['Feed the pets',
+                   'Take the dog out for a walk']
   },
   {
     id: 14,
