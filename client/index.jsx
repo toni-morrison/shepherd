279d706c6c966eaf26f701591367e4465e4408b4
Daniel Guan <daniel.guan.bca@gmail.com>
2018-08-14 14:46:34 -0400

    Merge branch 'dev' of https://github.com/toni-morrison/shepherd into sitterHome

--- client/index.jsx
+++ client/index.jsx
@@ -4,9 +4,11 @@ import axios from 'axios';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider, Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import TopTabs from './Tabs/TopTabs.jsx'
+import TopTabs from './Tabs/TopTabs.jsx';
 import Login from './Login/Login.jsx';
 import Signup from './Signup/Signup.jsx';
+// import Calendar from "./Calendar/Calendar.jsx"
+import UserSitterRequest from './UserSitterRequest/UserSitterRequest.jsx';
 
 const client = new ApolloClient();
 
@@ -21,9 +23,12 @@ class App extends React.Component {
     return (
       <ApolloProvider client={client}>
         <div>Shepherd</div>
-        <Login />
-        <Signup />
+
+        {/* <Login />
+        <Signup /> */}
+        {/* <Calendar/> */}
         <TopTabs />
+        <UserSitterRequest />
       </ApolloProvider>
     );
   }
