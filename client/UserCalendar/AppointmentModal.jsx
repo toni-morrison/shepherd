acb3b636174eb8dc36e2ea29dc2a31a0cc16b10e
Kiernan Hahn <kiernanhahn@gmail.com>
2018-08-15 11:28:23 -0400

    Finished the basic front-end user Calendar component

--- client/UserCalendar/AppointmentModal.jsx
+++ client/UserCalendar/AppointmentModal.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import {Modal} from 'react-bootstrap'
 function AppointmentModal (props) {
+  let trackerObj = ''
+  if (props.currentEvent.instructions) {
+    props.currentEvent.instructions.forEach ((instruction) => trackerObj += instruction)
+  }
+  console.log (trackerObj)
   return (
     <Modal show = {props.show} onHide = {props.handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>{props.currentEvent.username}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        Status:     {props.currentEvent.status}
-        Start Time: {props.currentEvent.start}
-        Cost:       ${props.currentEvent.cost}
+        <div> Status:     {props.currentEvent.status} </div>
+        <div> Start Time: {props.currentEvent.start ? props.currentEvent.start.toLocaleDateString () + ', ' + props.currentEvent.start.toLocaleTimeString () : ''} </div>
+        <div> Cost:       ${props.currentEvent.cost}</div>  
+        <div> Instructions:
+          <ul>
+            {  
+              (props.currentEvent.instructions) ?
+                props.currentEvent.instructions.map (function (instruction) {return (<li>{instruction}</li>)})
+                : <li>Instruction Here</li>
+            }
+          </ul>
+        </div>
       </Modal.Body>
+      
     </Modal>
   )
 }
