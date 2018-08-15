0cee9070af33d86a2401625384dadb97e16b645d
Dan Guan <daniel.guan.bca@gmail.com>
2018-08-15 11:57:46 -0400

    Server scaffolding

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
 
 const app = express();
 
-app.use(express.static(__dirname + '/../../client'));
+app.use(express.static(__dirname, 'public'));
 
 
 const port = process.env.PORT || 3000;
