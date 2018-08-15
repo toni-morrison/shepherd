d03f260c5bafbc68df2c2544fea71a14806c6816
Dan Guan <daniel.guan.bca@gmail.com>
2018-08-15 13:50:53 -0400

    Fix styles and server
    - Changed server port back to 3000
    - Split styles to 2 files

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.static(__dirname + '/../../client'));
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Listening on port: ', port));
