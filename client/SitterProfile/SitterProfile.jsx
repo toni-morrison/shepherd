4d49b1f37454539511f98204f00a644628afb5c8
Debbie Lee <ydebbielee@gmail.com>
2018-08-21 11:08:41 -0400

    feat: Edited some styling in SitterProfile

--- client/SitterProfile/SitterProfile.jsx
+++ client/SitterProfile/SitterProfile.jsx
@@ -31,12 +31,28 @@ export default class SitterProfile extends React.Component {
               <Col xs={12}>
               <center><h3>Personal Information</h3></center>
                 <Well bsSize="large" style={{ width:'100%' }}>
-                  {/* PULL ALL SITTER INFO FROM USER PROFILE */}
                   {/* PHOTO OF USER */}
-                  <h4>Name:</h4><p>Debbie</p><br/>
-                  <h4>Email: </h4><p>debbie@sitter.com</p><br/>
-                  <h4>Address: </h4><p>369 Lexington Ave, New York, New York</p><br/>
-                  <h4>Bio: </h4><p>hi</p>
+                  <Row>
+                    <Col xs={12}>
+                      <h4><strong>Name: </strong>Debbie</h4>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={12}>
+                      <h4><strong>Email:  </strong>debbie@sitter.com</h4>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={12}>
+                      <h4><strong>Address: </strong>369 Lexington Ave, New York, New York</h4>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={12}>
+                      <h4><strong>Bio: </strong>hi</h4>
+                    </Col>
+                  </Row>
+                  <br/>
                   <Button onClick={this.handleInfoUpdate}>Click to Update</Button>
                 </Well>
               </Col>
