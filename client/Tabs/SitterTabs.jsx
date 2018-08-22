24c6b1887ee22561ac6dbae9936484ae1df83f04
Daniel Guan <daniel.guan.bca@gmail.com>
2018-08-22 13:45:50 -0400

    feat(UserInfo): Retrieve/pass down sitterId when user is logged in
    Call getUserInfo query to retrieve sitter ID to pass down to all components

--- client/Tabs/SitterTabs.jsx
+++ client/Tabs/SitterTabs.jsx
@@ -21,40 +21,44 @@ export default class SitterTabs extends React.Component {
   }
 
   render() {
-    return (
-      <Tab.Container id="sitter-tabs" defaultActiveKey="first">
-        <Row className="clearfix">
-          <Col sm={2}>
-            <Nav bsStyle="pills" stacked>
-              <NavItem eventKey="first">HOME</NavItem>
-              <NavItem eventKey="second">METRICS</NavItem>
-              <NavItem eventKey="third">REVIEWS</NavItem>
-              <NavItem eventKey="fourth">SCHEDULE</NavItem>
-              <NavItem eventKey="fifth">PROFILE</NavItem>
-              <NavItem onClick={() => this.sitLogOut()}>LOGOUT</NavItem>
-            </Nav>
-          </Col>
-          <Col sm={8}>
-            <Tab.Content>
-              <Tab.Pane eventKey="first">
-                <SitterHome />
-              </Tab.Pane>
-              <Tab.Pane eventKey="second">
-                <SitterMetrics />
-              </Tab.Pane>
-              <Tab.Pane eventKey="third">
-                <SitterReviews />
-              </Tab.Pane>
-              <Tab.Pane eventKey="fourth">
-                <SitterSchedule />
-              </Tab.Pane>
-              <Tab.Pane eventKey="fifth">
-                <SitterProfile />
-              </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
-    );
+    if (!this.props.sitterId) {
+      return null;
+    } else {
+      return (
+        <Tab.Container id="sitter-tabs" defaultActiveKey="first">
+          <Row className="clearfix">
+            <Col sm={2}>
+              <Nav bsStyle="pills" stacked>
+                <NavItem eventKey="first">HOME</NavItem>
+                <NavItem eventKey="second">METRICS</NavItem>
+                <NavItem eventKey="third">REVIEWS</NavItem>
+                <NavItem eventKey="fourth">SCHEDULE</NavItem>
+                <NavItem eventKey="fifth">PROFILE</NavItem>
+                <NavItem onClick={() => this.sitLogOut()}>LOGOUT</NavItem>
+              </Nav>
+            </Col>
+            <Col sm={8}>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <SitterHome />
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <SitterMetrics />
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  <SitterReviews />
+                </Tab.Pane>
+                <Tab.Pane eventKey="fourth">
+                  <SitterSchedule />
+                </Tab.Pane>
+                <Tab.Pane eventKey="fifth">
+                  <SitterProfile />
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      );
+    }
   }
 }
