2f676fc0d665ac79c53ca525c9c8225b43e126c0
Dan Guan <daniel.guan.bca@gmail.com>
2018-08-29 22:15:55 -0400

    fix(Calendar): Fix dynamic content to show upon clicking a transaction
    Fixed sitter calendar modal when clicking on a transaction in sitter calendar

--- client/SitterSchedule/CalendarQuery.jsx
+++ client/SitterSchedule/CalendarQuery.jsx
@@ -1,105 +1,129 @@
-import React from 'react'
+import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-import { ApolloProvider } from "react-apollo";
 
-const FIND_SITTER_APPOINTMENTS = gql `
-  query findSitterAppointments ($sitterEmail: String!) {
-    findSitterAppointments (sitterEmail: $sitterEmail) {
-      start  
+const FIND_SITTER_APPOINTMENTS = gql`
+  query findSitterAppointments($sitterEmail: String!) {
+    findSitterAppointments(sitterEmail: $sitterEmail) {
+      start
       end
       day
       appointment {
         id
         price
         children
-        pets  
+        pets
         comment
         app_types
         status
+        userRating
+        userReview
         todoList {
           id
+          name
         }
-        sitterRating
         sitter {
           id
+          rating
           rates {
             child_rate
             pet_rate
             home_rate
           }
           user {
-            pic_url
             email
             first_name
             last_name
+            rating
           }
         }
         user {
+          first_name
+          last_name
           email
           id
+          pic_url
         }
       }
     }
   }
 `;
-function CalendarQuery (props) {
-  return (<Query 
-            query = {FIND_SITTER_APPOINTMENTS} 
-            variables = {{sitterEmail: props.user}} >
-        {
-          ({ loading, error, data }) => {
-            if (loading) {
-              return <span></span>
-            }
-            if (error) {
-              return <span></span>
-            }
-            console.log ('data, Sitter Query: ', data)
-            let tempData = []
-            data.findSitterAppointments.map (
-              function (timeInt) {
-                let startMin = timeInt.start % 60;
-                let endMin = timeInt.end % 60;
-                startMin = (startMin < 10 ? '0' + startMin : '' + startMin)
-                endMin = (endMin < 10 ? '0' + endMin : '' + endMin)
-                let startHour = Math.floor (timeInt.start / 60);
-                let endHour = Math.floor (timeInt.end / 60);
-                startHour = (startHour < 10 ? '0' + startHour : '' + startHour)
-                endHour = (endHour < 10 ? '0' + endHour : '' + endHour)
-                let startTime = timeInt.day + 'T' + startHour + ':' + startMin + ':00'
-                let endTime = timeInt.day + 'T' + endHour + ':' + endMin + ':00'
-                startTime = new Date (startTime)
-                endTime = new Date (endTime)
-                console.log ('startTime: ', startTime)
-                console.log ('endTime: ', endTime)
-                let cost = 0;
-                for (var i = 0; i < timeInt.appointment.app_types.length; i++) {
-                  cost += ((timeInt.start - timeInt.End) / 60) * timeInt.appointment.sitter.rates [timeInt.appointment.app_types[i] + '_rate']
-                }
-                tempData.push ({
-                  allDay: false,
-                  cost: timeInt.appointment.price,
-                  appointmentID: timeInt.appointment.id,
-                  start: startTime,
-                  end: endTime,
-                  userID: timeInt.appointment.user.id,
-                  sitterID: timeInt.appointment.sitter.id,
-                  status: timeInt.appointment.status,
-                  username: timeInt.appointment.sitter.user.first_name + ' ' + timeInt.appointment.sitter.user.last_name,
-                  instructionID: (timeInt.appointment.todoList !== null ? timeInt.appointment.todoList.id : null),
-                  sittername: timeInt.appointment.sitter.user.first_name + ' ' + timeInt.appointment.sitter.user.last_name,
-                  comment: timeInt.appointment.comment,
-                  rating: timeInt.appointment.sitterRating,
-                  pic_url: timeInt.appointment.sitter.user.pic_url
-                })
-              }
-            )
-            console.log ('tempData, Sitter: ', tempData)
-            props.handleQuery (tempData)
-            return <span></span>
-          }
+
+const FIND_INSTRUCTIONS = gql`
+  query findInstructions($id: ID!) {
+    findInstructions(id: $id) {
+      time
+      desc
+    }
+  }
+`;
+
+function CalendarQuery(props) {
+  return (
+    <Query
+      query={FIND_SITTER_APPOINTMENTS}
+      variables={{ sitterEmail: props.user }}
+    >
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <span />;
         }
-      </Query>)
+        if (error) {
+          console.log(error);
+          return <span />;
+        }
+
+        let tempData = [];
+        data.findSitterAppointments.map(function(timeInt) {
+          let startMin = timeInt.start % 60;
+          let endMin = timeInt.end % 60;
+          startMin = startMin < 10 ? '0' + startMin : '' + startMin;
+          endMin = endMin < 10 ? '0' + endMin : '' + endMin;
+          let startHour = Math.floor(timeInt.start / 60);
+          let endHour = Math.floor(timeInt.end / 60);
+          startHour = startHour < 10 ? '0' + startHour : '' + startHour;
+          endHour = endHour < 10 ? '0' + endHour : '' + endHour;
+          let startTime =
+            timeInt.day + 'T' + startHour + ':' + startMin + ':00';
+          let endTime = timeInt.day + 'T' + endHour + ':' + endMin + ':00';
+          startTime = new Date(startTime);
+          endTime = new Date(endTime);
+
+          tempData.push({
+            allDay: false,
+            cost: timeInt.appointment.price,
+            appointmentID: timeInt.appointment.id,
+            start: startTime,
+            end: endTime,
+            userID: timeInt.appointment.user.id,
+            sitterID: timeInt.appointment.sitter.id,
+            status: timeInt.appointment.status,
+            username:
+              timeInt.appointment.user.first_name +
+              ' ' +
+              timeInt.appointment.user.last_name,
+            instructionsID: timeInt.appointment.todoList
+              ? timeInt.appointment.todoList.id
+              : undefined,
+            instructionsName: timeInt.appointment.todoList
+              ? timeInt.appointment.todoList.name
+              : undefined,
+            sittername:
+              timeInt.appointment.sitter.user.first_name +
+              ' ' +
+              timeInt.appointment.sitter.user.last_name,
+            comment: timeInt.appointment.comment,
+            userAppRating: timeInt.appointment.userRating,
+            userAppReview: timeInt.appointment.userReview,
+            sitterRating: timeInt.appointment.sitter.rating,
+            userRating: timeInt.appointment.user.rating,
+            pic_url: timeInt.appointment.user.pic_url
+          });
+        });
+        props.handleQuery(tempData);
+        return <span />;
+      }}
+    </Query>
+  );
 }
-export default CalendarQuery
+export default CalendarQuery;
