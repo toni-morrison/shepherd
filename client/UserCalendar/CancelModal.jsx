88f6c4b28891ef435a2e604784d2d10ca328efd4
Dan Guan <daniel.guan.bca@gmail.com>
2018-08-30 19:07:05 -0400

    style(Calendar): Style and refactor user calendar components

--- client/UserCalendar/CancelModal.jsx
+++ client/UserCalendar/CancelModal.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import {
-  Modal,
-  Button}
-from 'react-bootstrap'
-import CancelMutation from './CancelMutation.jsx'
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import CancelMutation from './CancelMutation.jsx';
 
-function CancelModal (props) {
+function CancelModal(props) {
   return (
-    <Modal show = {props.show} onHide = {props.handleClose}>
-      <Modal.Body>Are you sure you want to cancel this appointment?</Modal.Body>
-      <CancelMutation 
-        apntID = {props.event.appointmentID}
-        handleClose = {props.handleClose}/>
-      <Button 
-        onClick = {props.handleClose}>
-        No, Do Not Cancel</Button>
+    <Modal show={props.show} onHide={props.handleClose}>
+      <Modal.Body>
+        {props.event.status === 'Rejected'
+          ? 'Are you sure you want to remove this appointment?'
+          : 'Are you sure you want to cancel this appointment?'}
+      </Modal.Body>
+      <CancelMutation
+        apntID={props.event.appointmentID}
+        handleClose={props.handleClose}
+      />
+      <Button onClick={props.handleClose}>
+        {props.event.status === 'Rejected'
+          ? 'No, do not remove'
+          : 'No, do not cancel'}
+      </Button>
     </Modal>
-  )
+  );
 }
-export default CancelModal
+export default CancelModal;
